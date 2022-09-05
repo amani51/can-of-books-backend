0dcc9a09d8d7cd9dffa59701e7b1ff7d6f822f66
amani51 <amani.898919@gmail.com>
2022-09-06 01:43:16 +0300

    class12

--- server.js
+++ server.js
@@ -6,12 +6,16 @@ const cors = require("cors");
 const mongoose = require("mongoose");
 const app = express();
 app.use(cors());
-
+app.use(express.json());
 const PORT = process.env.PORT || 3001;
 let Book = require("./schema");
 
 //------MONGOOSE CONFIGURATION-------//
-mongoose.connect("mongodb://localhost:27017/Book", {
+// mongoose.connect("mongodb://localhost:27017/Book", {
+//   useNewUrlParser: true,
+//   useUnifiedTopology: true,
+// });
+mongoose.connect(`${process.env.mongodb_url}`, {
   useNewUrlParser: true,
   useUnifiedTopology: true,
 });
@@ -22,21 +26,21 @@ async function seedData() {
     title: "Quran",
     description:
       "The Quran, also romanized Qur'an or Koran, is the central religious text of Islam, believed by Muslims to be a revelation from God. It is organized into 114 chapters, which consist of verses",
-    status: "Religious ",
+    status: "Favorite Five",
   });
 
   const secondBook = new Book({
     title: "Ar-Raheeq- ul-Makhtum",
     description:
       "Published in 1983 this book is a biography of Messenger Mohammad PBUH. The Book revolves around old Arab sources that go back to the 9th century, and some of the chapters are translated for the first time. So the book offers new insights and details. The biography contains 85 chapters and is translated into many languages.",
-    status: "Autobiography",
+    status: "Favorite Five",
   });
 
   const thirdBook = new Book({
     title: "Loving our Parents",
     description:
       "The book is a collection of stories from people regarding treating our parents with respect and honor. Abdul Malik Mujahid details our duties and obligations to our parents in the light of Sunnah who has sacrificed to raise and educate us.",
-    status: "educational",
+    status: "Recommended To Me",
   });
 
   await firstBook.save();
@@ -48,6 +52,8 @@ async function seedData() {
 app.get("/", getHomeHandler);
 app.get("/test", getTestHandler);
 app.get("/books", getBookHandler);
+app.post("/books", addBookHandler);
+app.delete('/books/:id',deleteBookHandler);
 app.get("*", getDefaultHandler);
 
 //------HANDLER SECTIONS-------//
@@ -77,6 +83,48 @@ function getBookHandler(req,res) {
   })
 }
 
+// http://localhost:3001/books // addBooks
+// http://localhost:3001/books?title=vghv&description=jhjiu&status=ljhbgjb
+async function addBookHandler(req,res){
+  console.log(req.body)
+  const {title,description,status} = req.body
+ await Book.create({
+    title: title,
+    description:description,
+    status:status ,
+  })
+  Book.find({},(err,result)=>{
+    if(err)
+    {
+        console.log(err);
+    }
+    else
+    {
+        console.log(result);
+        res.json(result);
+    }
+})
+}
+
+function deleteBookHandler(req,res){
+  const bookId = req.params.id;
+  console.log(bookId)
+  Book.deleteOne({_id:bookId},(err,result)=>{
+      
+    Book.find({},(err,result)=>{
+      if(err)
+      {
+          console.log(err);
+      }
+      else
+      {
+          console.log(result);
+          res.json(result);
+      }
+  })
+
+  })
+}
 // http://localhost:3001/*
 function getDefaultHandler(req, res) {
   res.status(404).send("SORRY! PAGE IS NOT FOUND");
